8bdac084d88e3c7214892fa52357eb8543d64a88
Piotr <piotr.bania@europe.com>
2022-08-22 12:18:17 +0100

    init

--- src/scripts/levels/level.js
+++ src/scripts/levels/level.js
@@ -1,12 +1,88 @@
 import * as THREE from 'three'
-
 import scene from '../core/setup'
+import {
+    GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader'
 
-const geometry = new THREE.BoxGeometry(2.5, 0.1, 0.2)
-const material = new THREE.MeshStandardMaterial({
-    color: 0x0000ff
+// Grass modelimport
+import Grass from '../../assets/grass.glb'
+
+let grass = new GLTFLoader()
+grass.load(Grass, function (gltf) {
+    grass = gltf.scene
+    gltf.scene.scale.set(1, 1, 1)
+    gltf.scene.position.set(-1, -1, 0)
+
+    const newMaterial = new THREE.MeshPhysicalMaterial({
+        color: 0x61F570,
+        metalness: 0,
+        roughness: 1,
+        transmission: 0.1,
+        thickness: 1,
+    })
+    grass.traverse((o) => {
+        if (o.isMesh) o.material = newMaterial
+    })
+    grass.traverse(n => {
+        if (n.isMesh) {
+            n.castShadow = true
+            n.receiveShadow = true
+            if (n.material.map) n.material.map.anisotropy = 16
+        }
+    })
+
+    scene.add(grass)
 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
 
-cube.position.set(0, -1, 0)
+let grass1 = new GLTFLoader()
+grass1.load(Grass, function (gltf) {
+    grass1 = gltf.scene
+    gltf.scene.scale.set(1, 1, 1)
+    gltf.scene.position.set(0, -1, 0)
+    
+    const newMaterial = new THREE.MeshPhysicalMaterial({
+        color: 0x61F570,
+        metalness: 0,
+        roughness: 1,
+        transmission: 0.1,
+        thickness: 1,
+    })
+    grass1.traverse((o) => {
+        if (o.isMesh) o.material = newMaterial
+    })
+    grass1.traverse(n => {
+        if (n.isMesh) {
+            n.castShadow = true
+            n.receiveShadow = true
+            if (n.material.map) n.material.map.anisotropy = 16
+        }
+    })
+    scene.add(grass1)
+})
+
+let grass2 = new GLTFLoader()
+grass2.load(Grass, function (gltf) {
+    grass2 = gltf.scene
+    gltf.scene.scale.set(1, 1, 1)
+    gltf.scene.position.set(1, -0.95, 0)
+    gltf.scene.rotation.set(0, 0, 0.1)
+    
+    const newMaterial = new THREE.MeshPhysicalMaterial({
+        color: 0x61F570,
+        metalness: 0,
+        roughness: 1,
+        transmission: 0.1,
+        thickness: 1,
+    })
+    grass2.traverse((o) => {
+        if (o.isMesh) o.material = newMaterial
+    })
+    grass2.traverse(n => {
+        if (n.isMesh) {
+            n.castShadow = true
+            n.receiveShadow = true
+            if (n.material.map) n.material.map.anisotropy = 16
+        }
+    })
+    scene.add(grass2)
+})
